c42f62cf8ccf567949330ed96948060ec61d7eda
KraivenR <ribeirokraiven@gmail.com>
2025-05-26 11:14:06 +0000

    save the file

---
+++ practice_tasks/functions.py
@@ -0,0 +1,8 @@
+# A function is a block of code which only runs when it is called.
+def create_event(name, payload):                             #In Python a function is defined using the def keyword 
+       return {"Event" : name, "Event_details":payload }     #return is  a keyword that hands the result back so it can be used or saved later
+    
+
+
+
+
